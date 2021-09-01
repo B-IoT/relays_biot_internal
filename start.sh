b7d2e735ba7c831d829c09d88a9bbeb220b783bf
samuelchassot <samuel.chassot@epfl.ch>
2021-09-01 08:36:02 +0200

    typo

--- start.sh
+++ start.sh
@@ -7,5 +7,5 @@ while ! (ping -c 1 -W 1 1.2.3.4 | grep -q 'statistics'); do
     sleep 10
 done
 
-pi pip3 install -r requirements.txt
-pi python3 emergency_web_client.py >> /home/pi/biot/logs/emergency_web_client.py
+pip3 install -r requirements.txt
+python3 emergency_web_client.py >> /home/pi/biot/logs/emergency_web_client.py
